69a393fb635b5e1fd14190a6bc6a84d1fd720d45
vitalis.popoff <vitalis.popoff@gmail.com>
2020-01-09 15:14:34 +0100

    pom update + first attempts in excs

---
+++ src/main/java/AlgebraicOperational.java
@@ -0,0 +1,8 @@
+public interface AlgebraicOperational {
+
+    static int add(int x, int y){return 0;}
+    static int subtract(int x, int y){return 0;}
+    static int multiply(int x, int y){return 0;}
+    static int divide(int x, int y){return 0;}
+
+}
---
+++ src/test/java/AlgebraicOperationalTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+public class AlgebraicOperationalTest {
+
+    @Test
+    public void addTest(){
+        Assertions.assertEquals(0,AlgebraicOperational.add(0,0));
+    }
+
+}
