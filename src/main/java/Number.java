727962efe8198397898506ee8e43cdf17434a377
vitalis.popoff <vitalis.popoff@gmail.com>
2020-01-11 05:37:34 +0100

    in a middle of Number class construction

--- src/main/java/Number.java
+++ src/main/java/Number.java
@@ -4,18 +4,43 @@ import java.util.List;
 public class Number {
 
 
-    static int convertToNumber(String a) {
-        return 0;
+    static double convertToNumber(String a) {
+        double result = 0.;
+        List<Character> charSeries = sliceToSeries(a);
+        if (isSeriesProperNumber(charSeries)) {
+
+        }
+        return result;
     }
 
     static List<Character> sliceToSeries(String a) {
         List<Character> charSeries = new ArrayList<>();
         for (int i = 0; i < a.length(); i++) {
             char c = a.charAt(i);
-            if (InputDataStreamReading.isInt(c) || InputDataStreamReading.isRadixPoint(c))
-                charSeries.add(c);
+            charSeries.add(c);
         }
         return charSeries;
     }
 
+    static boolean isSeriesProperNumber(List<Character> charSeries) {
+        boolean result = true;
+        int radixPoints = 0;
+        for (char c : charSeries) {
+            if (InputDataStreamReading.isRadixPoint(c)) radixPoints++;
+            if ((!InputDataStreamReading.isInt(c) && radixPoints > 1) || charSeries.size() == radixPoints)
+                result &= false;
+        }
+        return result;
+    }
+
+    static double extractIntegerPartOfCharSeries(List<Character> charSeries) {
+        double result = 0.;
+        int numberLength = charSeries.indexOf('.') - 1;
+
+        for (int i = 0; i <= numberLength; i++) {
+            int digit = InputDataStreamReading.toInt(charSeries.get(i));
+            result += digit * Math.pow(10, numberLength - i);
+        }
+        return result;
+    }
 }
--- src/test/java/NumberTest.java
+++ src/test/java/NumberTest.java
@@ -8,15 +8,53 @@ import java.util.List;
 
 public class NumberTest {
 
-//    @Ignore
-//    @Test
+    @Ignore
+    @Test
     public void convertToNumberTest01(){
-        Assert.assertEquals(1.1f, Number.convertToNumber("1.1"));
+        Assert.assertEquals(1.1d, Number.convertToNumber("1.1"), 0.0);
     }
 
     @Test
-    public void  sliceToSeriesTest01(){
+    public void sliceToSeriesTest01(){
         List<Character> result = new ArrayList<Character>(Arrays.asList('1','.','1'));
         Assert.assertEquals(result, Number.sliceToSeries("1.1"));
     }
+
+    @Test
+    public void sliceToSeriesTest02(){
+        List<Character> result = new ArrayList<Character>(Arrays.asList('1','a','.','1'));
+        Assert.assertEquals(result, Number.sliceToSeries("1a.1"));
+    }
+
+    @Test
+    public void isSeriesProperNumberTest01(){
+        Assert.assertTrue(Number.isSeriesProperNumber(Number.sliceToSeries("1.1")));
+    }
+
+    @Test
+    public void isSeriesProperNumberTest02(){
+        Assert.assertFalse(Number.isSeriesProperNumber(Number.sliceToSeries("1..1")));
+    }
+
+    @Test
+    public void isSeriesProperNumberTest03(){
+        Assert.assertFalse(Number.isSeriesProperNumber(Number.sliceToSeries(".")));
+    }
+
+    @Test
+    public void extractIntegerPartOfCharSeriesTest01(){
+        List<Character> result = new ArrayList<Character>(Arrays.asList('1','1','.','1'));
+        Assert.assertEquals(11.d, Number.extractIntegerPartOfCharSeries(result),0.d);
+    }
+
+//  TODO toggle the test, build the function.
+//    @Test
+    public void extractFractionPartOfCharSeriesTest01(){
+        List<Character> result = new ArrayList<Character>(Arrays.asList('1','.','1','1'));
+//        Assert.assertEquals(0.11d, Number.extractFractionPartOfCharSeries(result),0.d);
+    }
+
+
+
+
 }
