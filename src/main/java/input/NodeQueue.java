f2675cb1fa24b7c12211947d98d1c4a8b38125e0
vitalispopoff <vitalis.popoff@gmail.com>
2020-04-26 21:59:33 +0200

    NodeQueue > convertToLocalTree() clearing

--- src/main/java/input/NodeQueue.java
+++ src/main/java/input/NodeQueue.java
@@ -14,42 +14,29 @@ class NodeQueue extends Queueability implements Queuing {
     public void convertToLocalTree(){
 
         /* 1.1 :    extract queue.head from queue */      // * queue.head == opLeft now
-
-        Enqueued opLeft = updateQueue();
-
         /* 1.2 :    analyze opLeft  */
-
-        Nodeable opLeftNode = opLeft.getNode();
-        Typical opLeftNodeType = opLeftNode.getType();
-
         /* 1.3 :    is opLeft a value ? */
-
-        boolean isLeftNodeValue = opLeftNodeType == NodeType.VALUE;
-
         /* 2.1 :    extract queue.head from queue */      // * queue.head == opRoot now
-
-        Enqueued opRoot = updateQueue();
-
         /* 2.2 :    analyze opRoot */
-
-        Nodeable opRootNode = opRoot.getNode();
-        Typical opRootNodeType = opRootNode.getType();
-
         /* 2.3 :    is opRoot a prioritized operation ? */
-
-        boolean isOpRootProper = isQueueNodePrioritized(opRootNodeType);
-
         /* 3.1 :    extract queue head from queue */      // * queue.head == opRite now
-
-        Enqueued opRite = updateQueue();
-
         /* 3.2 :    analyze opRite */
-
-        Nodeable opRiteNode = opRite.getNode();
-        Typical opRiteNodeType = opRiteNode.getType();
-
         /* 3.3 :    is oprite a value ? */
 
+        Enqueued opLeft = updateQueue();
+        Enqueued opRoot = updateQueue();
+        Enqueued opRite = updateQueue();
+
+        Nodeable opLeftNode = opLeft.getNode();
+        Nodeable opRootNode = opRoot.getNode();
+        Nodeable opRiteNode = opRite.getNode();
+
+        Typical opLeftNodeType = opLeftNode.getType();
+        Typical opRootNodeType = opRootNode.getType();
+        Typical opRiteNodeType = opRiteNode.getType();
+
+        boolean isLeftNodeValue = opLeftNodeType == NodeType.VALUE;
+        boolean isOpRootProper = isQueueNodePrioritized(opRootNodeType);
         boolean isRiteNodeValue = opRiteNodeType == NodeType.VALUE;
 
         /* 4. :    if 1.3 , 2.3, 3.3 are true then make them a local tree*/
