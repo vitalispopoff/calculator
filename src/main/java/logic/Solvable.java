e9a07f4ac15b0bb1b92ffe6cb753a70ad131f28b
vitalispopoff <vitalis.popoff@gmail.com>
2020-05-15 11:06:04 +0200

    adding data package scheme

--- src/main/java/logic/Solvable.java
+++ src/main/java/logic/Solvable.java
@@ -4,23 +4,7 @@ public interface Solvable /*extends Queueable*/ {
 
 //	@formatter:off
 
-	/*default*/ void solve () /*{ convertToLocalTree ( getHead ()); }*/;
-
-	Solvable/*default Queueable*/ convertToLocalTree /*(Queueable queueable) {
-		if ( isSingle ())
-			return ( getHead ());
-		else {
-			if (search (queueable))
-				collapseToLocalTree (queueable);
-			else
-				return convertToLocalTree (getTail ());
-		}
-		return null;
-	}*/(Solvable solvable);
-
-	/*default*/ boolean search/* (Queueable queueable) { return false; }*/(Solvable solvable);
-
-	/*default*/ void collapseToLocalTree /*(Queueable queueable) { }*/(Solvable solvable);
+	String solve ();
 
 //	@formatter:on
 
