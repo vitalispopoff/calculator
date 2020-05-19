ca6fb0b99b4282f7bb665fab5f0eefb58fa1db0e
vitalispopoff <vitalis.popoff@gmail.com>
2020-05-19 22:57:54 +0200

    backup before rebuilding Queueable inner structure

--- src/main/java/data/Enqueued.java
+++ src/main/java/data/Enqueued.java
@@ -20,15 +20,16 @@ public interface Enqueued extends Queueable {
 
 
 
+
 		return null;
 	}
 
 	@Override int getTypeIndex();
 
-	@Override default void setHead (Queueable queueable) { /*setPrev (queueable);*/ }
+	@Override default void setHead (Queueable queueable) { setPrev (queueable); }
 	@Override default void setTail (Queueable queueable) { setPost (queueable); }
 
-	@Override default Queueable getHead () { return null/*getPrev ()*/; }
+	@Override default Queueable getHead () { return getPrev (); }
 	@Override default Queueable getTail () { return getPost (); }
 
 //	@formatter:on
